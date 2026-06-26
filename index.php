a8b67bcd4c962b5ca7a83697455259db5914576c
jm-Quezada <jm_soto@marianosamaniego.edu.ec>
2026-06-26 18:37:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,14 @@ require_once "controladores/ctrlUsurios.php";
 require_once "controladores/ctrlPersona.php";
 require_once "controladores/ctrlGeneroPeliculas.php";
 require_once "controladores/ctrlinvitados.php";
-require_once "controladores/ctrlEstudiantes.php";
-require_once "controladores/ctrlEventos.php";
+
 
 //llamar a todos los modelos
 require_once "modelos/mdlUsuario.php";
 require_once "modelos/mdlPersona.php";
 require_once "modelos/mdlGeneroPeliculas.php";
 require_once "modelos/mdlinvitado.php";
-require_once "modelos/mdlEstudiantes.php";
-require_once "modelos/mdlEventos.php";
+
+
 $objPrincipal = new Principal();
 $objPrincipal->ctrlPrincipal();
